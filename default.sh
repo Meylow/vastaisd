1f643e7d74cbf232a9014f82e843e76c9f8b9ae6
Meylow <meylowty@gmail.com>
2025-06-15 08:00:47 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -16,10 +16,10 @@ PIP_PACKAGES=(
 
 CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
-    "https://civitai.com/api/download/models/501240?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-    "https://civitai.com/api/download/models/290640?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-    "https://civitai.com/api/download/models/143906?type=Model&format=SafeTensor&size=pruned&fp=fp16"
-    "https://civitai.com/api/download/models/1838857?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+    "https://civitai.com/api/download/models/501240"
+    "https://civitai.com/api/download/models/290640"
+    "https://civitai.com/api/download/models/143906"
+    "https://civitai.com/api/download/models/1838857"
 )
 
 UNET_MODELS=(
