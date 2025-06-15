21c13ef0de77416e9931332940f4d15e1096f62c
Meylow <meylowty@gmail.com>
2025-06-15 10:48:25 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -3,17 +3,16 @@
 source /venv/main/bin/activate
 A1111_DIR=${WORKSPACE}/stable-diffusion-webui
 
-# Packages are installed after nodes so we can fix them...
-
-APT_PACKAGES=(
-    #"package-1"
-    #"package-2"
+# Optional: Extensions
+EXTENSIONS=(
+    "https://github.com/Mikubill/sd-webui-controlnet"
 )
 
-PIP_PACKAGES=(
+# Optional APT/PIP
+APT_PACKAGES=()
+PIP_PACKAGES=()
 
-)
-
+# Model-Download-Listen
 CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
     "https://civitai.com/api/download/models/501240"
@@ -22,33 +21,27 @@ CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/1838857"
 )
 
-UNET_MODELS=(
-  
-)
-
 LORA_MODELS=(
-    "https://huggingface.co/h94/IP-Adapter-FaceID/blob/main/ip-adapter-faceid-plusv2_sd15_lora.safetensors"
-    "https://huggingface.co/h94/IP-Adapter-FaceID/blob/main/ip-adapter-faceid-plusv2_sdxl_lora.safetensors"
+    "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sd15_lora.safetensors"
+    "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sdxl_lora.safetensors"
 )
 
 VAE_MODELS=(
-    "https://huggingface.co/stabilityai/sdxl-vae/blob/main/sdxl_vae.safetensors"
+    "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
 )
 
 ESRGAN_MODELS=(
-    "https://huggingface.co/uwg/upscaler/blob/main/ESRGAN/4x_foolhardy_Remacri.pth"
-    "https://huggingface.co/uwg/upscaler/blob/main/ESRGAN/4x_NickelbackFS_72000_G.pth"
-    "https://huggingface.co/uwg/upscaler/blob/main/ESRGAN/4x_NMKD-Siax_200k.pth"
-    "https://huggingface.co/uwg/upscaler/blob/main/ESRGAN/4x_NMKD-Superscale-SP_178000_G.pth"
-    "https://huggingface.co/uwg/upscaler/blob/main/ESRGAN/4x-UltraSharp.pth"
-    "https://huggingface.co/uwg/upscaler/blob/main/ESRGAN/8x_NMKD-Superscale_150000_G.pth"
+    "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_foolhardy_Remacri.pth"
+    "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NickelbackFS_72000_G.pth"
+    "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NMKD-Siax_200k.pth"
+    "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NMKD-Superscale-SP_178000_G.pth"
+    "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x-UltraSharp.pth"
+    "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/8x_NMKD-Superscale_150000_G.pth"
 )
 
 CONTROLNET_MODELS=(
-    "https://huggingface.co/h94/IP-Adapter-FaceID/blob/main/ip-adapter-faceid-plusv2_sd15.bin2"
-    "https://huggingface.co/h94/IP-Adapter-FaceID/blob/main/ip-adapter-faceid-plusv2_sdxl.bin"
-    "https://huggingface.co/lllyasviel/ControlNet-v1-1/tree/main"
-    
+    "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sd15.bin2"
+    "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sdxl.bin"
 )
 
 ### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
@@ -58,32 +51,16 @@ function provisioning_start() {
     provisioning_get_apt_packages
     provisioning_get_extensions
     provisioning_get_pip_packages
-    provisioning_get_files \
-        "${A1111_DIR}/models/Stable-diffusion" \
-        "${CHECKPOINT_MODELS[@]}"
-    provisioning_get_files \
-        "${A1111_DIR}/models/Lora" \
-        "${LORA_MODELS[@]}"
 
-    provisioning_get_files \
-        "${A1111_DIR}/models/VAE" \
-        "${VAE_MODELS[@]}"
+    provisioning_get_files "${A1111_DIR}/models/Stable-diffusion" "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_files "${A1111_DIR}/models/Lora" "${LORA_MODELS[@]}"
+    provisioning_get_files "${A1111_DIR}/models/VAE" "${VAE_MODELS[@]}"
+    provisioning_get_files "${A1111_DIR}/models/ESRGAN" "${ESRGAN_MODELS[@]}"
+    provisioning_get_files "${A1111_DIR}/extensions/sd-webui-controlnet/models" "${CONTROLNET_MODELS[@]}"
 
-    provisioning_get_files \
-        "${A1111_DIR}/models/ESRGAN" \
-        "${ESRGAN_MODELS[@]}"
-
-    provisioning_get_files \
-        "${A1111_DIR}/extensions/sd-webui-controlnet/models" \
-        "${CONTROLNET_MODELS[@]}"
-
-
-    
-    # Avoid git errors because we run as root but files are owned by 'user'
     export GIT_CONFIG_GLOBAL=/tmp/temporary-git-config
     git config --file $GIT_CONFIG_GLOBAL --add safe.directory '*'
 
-    # Start and exit because webui will probably require a restart
     cd "${A1111_DIR}"
     LD_PRELOAD=libtcmalloc_minimal.so.4 \
         python launch.py \
@@ -91,21 +68,21 @@ function provisioning_start() {
             --no-download-sd-model \
             --do-not-download-clip \
             --no-half \
-            --port 11404 \
-            --exit
+            --port 11404
 
     provisioning_print_end
 }
 
 function provisioning_get_apt_packages() {
     if [[ -n $APT_PACKAGES ]]; then
-            sudo $APT_INSTALL ${APT_PACKAGES[@]}
+        sudo apt-get update
+        sudo apt-get install -y ${APT_PACKAGES[@]}
     fi
 }
 
 function provisioning_get_pip_packages() {
     if [[ -n $PIP_PACKAGES ]]; then
-            pip install --no-cache-dir ${PIP_PACKAGES[@]}
+        pip install --no-cache-dir ${PIP_PACKAGES[@]}
     fi
 }
 
@@ -140,57 +117,22 @@ function provisioning_print_header() {
 }
 
 function provisioning_print_end() {
-    printf "\nProvisioning complete:  Application will start now\n\n"
+    printf "\nProvisioning complete: Application should be running now.\n\n"
 }
 
-function provisioning_has_valid_hf_token() {
-    [[ -n "$HF_TOKEN" ]] || return 1
-    url="https://huggingface.co/api/whoami-v2"
-
-    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
-        -H "Authorization: Bearer $HF_TOKEN" \
-        -H "Content-Type: application/json")
-
-    # Check if the token is valid
-    if [ "$response" -eq 200 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-function provisioning_has_valid_civitai_token() {
-    [[ -n "$CIVITAI_TOKEN" ]] || return 1
-    url="https://civitai.com/api/v1/models?hidden=1&limit=1"
-
-    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
-        -H "Authorization: Bearer $CIVITAI_TOKEN" \
-        -H "Content-Type: application/json")
-
-    # Check if the token is valid
-    if [ "$response" -eq 200 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Download from $1 URL to $2 file path
 function provisioning_download() {
     if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
         auth_token="$HF_TOKEN"
-    elif 
-        [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+    elif [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
         auth_token="$CIVITAI_TOKEN"
     fi
-    if [[ -n $auth_token ]];then
-        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    if [[ -n $auth_token ]]; then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes=4M -P "$2" "$1"
     else
-        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+        wget -qnc --content-disposition --show-progress -e dotbytes=4M -P "$2" "$1"
     fi
 }
 
-# Allow user to disable provisioning if they started with a script they didn't want
 if [[ ! -f /.noprovisioning ]]; then
     provisioning_start
 fi
